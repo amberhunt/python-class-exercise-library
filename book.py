5ea592c659551fbbdc8256324f09afe4853e2a4a
Eric Scace <escace@marlboro.edu>
2017-09-28 10:39:11 -0400

    Move check-out pseudocode to separate file
    Ref #12 #1

--- book.py
+++ book.py
@@ -10,18 +10,6 @@ class Book:
         hold_for: Person
 
     # methods:
-        check_out(Book, borrower, return_date)
-            # If the book is not alreayd borrowed...
-            # Is this a valid borrower
-            if (borrower is a Person who is_Borrower):
-                Book.borrower = borrower
-                # Make sure the return_date is in the future, but not too far into the future.
-                # Default is not more than 180 days.
-                if (return date is > 180 days):
-                    Print a message ... cannot borrow but for 180 days.
-                    Use the 180th day as the return date
-                Book.return_date = return_date
-            else (explain this person cannot borrow books)
 
         check_in(Book)
         hold(book, hold_for_person)
