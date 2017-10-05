ddee4bec9d7bba103e52dd2d12ea45c8b3b304f5
amberhunt <amber@marlboro.edu>
2017-10-05 09:53:38 -0400

    Add code for check_out
    Ref #28

--- book/check-out.py
+++ book/check-out.py
@@ -11,3 +11,21 @@
         #           Book.return_date = return_date
         #       no
         #           print message "Cannot checkout to non-borrower"
+from book import Book
+from person import Person
+from datetime import date
+from datetime import timedelta
+
+def check_out(self, me, borrower, return_date):
+    if me is Librarian :
+        if borrower is Borrower :
+            self.borrower = input("Enter borrower ")
+            custom = input("Enter Y for default, N for custom ")
+            if custom = "Y" :
+                self.return_date = str(date.today() + timedelta (days=180))
+            elif custom = "N" :
+                self.return_date = date(input("Enter date: yyyy/mm/dd "))
+        elif borrower is not Borrower :
+            print("Unknown borrower, please create a Person before check out")
+    elif me is not Librarian :
+        print("Non-librarians may not check out")
